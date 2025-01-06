544021ecf6aa0464beb42fb7cfa1f8d80dcfe75b
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-06 21:44:42 +0530

    Update __init__.py

--- Testing/__init__.py
+++ Testing/__init__.py
@@ -6,5 +6,5 @@ app = Client(
              api_id=Config.APP_ID, 
              api_hash=Config.HASH_ID, 
              bot_token=Config.TOKEN, 
-             plugins=dict(root="Testing")  # Ensure the plugins folder is structured correctly
+             plugins=dict(root="Testing.Modules")  # Ensure the plugins folder is structured correctly
 )
