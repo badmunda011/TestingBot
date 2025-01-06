2938c2a9ca3bfeb95fb3c7d8e7116ab8d71a6133
coixo <vivansingh8372@outlook.com>
2025-01-06 21:44:22 +0530

    Update __init__.py

--- Testing/__init__.py
+++ Testing/__init__.py
@@ -5,5 +5,6 @@ app = Client(
              name="TESTING", 
              api_id=Config.APP_ID, 
              api_hash=Config.HASH_ID, 
-             bot_token=Config.TOKEN
+             bot_token=Config.TOKEN, 
+             plugins=dict(root="Testing")  # Ensure the plugins folder is structured correctly
 )
