d33bea69ed5198291288245b79b51d6f6d76dfd8
coixo <vivansingh8372@outlook.com>
2025-01-06 19:54:14 +0530

    Create __init__.py

---
+++ Testing/__init__.py
@@ -0,0 +1,9 @@
+import Config
+from pyrogram import Client
+
+app = Client(
+             name="TESTING", 
+             api_id=Config.APP_ID, 
+             api_hash=Config.HASH_ID, 
+             bot_token=Config.TOKEN
+)
