c9285645d0001481b48a2bfe6722d162982422d0
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-11 09:34:42 +0530

    Update install.py

--- Testing/Modules/install.py
+++ Testing/Modules/install.py
@@ -122,3 +122,13 @@ async def uninstall_plugin(client, message):
     except Exception as e:
         await edit_or_reply(message, text=f"<b>Failed to uninstall plugin '{plugin_name}':</b>\n<pre>{str(e)}</pre>")
         logger.error(f"Failed to uninstall plugin '{plugin_name}': {str(e)}")
+
+# Restart command to ensure commands persist across restarts
+@app.on_message(filters.command("rs") & ~filters.forwarded & ~filters.via_bot)
+async def restart(client: PyroClient, message: Message):
+    reply = await message.reply_text("**🔁 Restarting...**")
+    await message.delete()
+    await reply.edit_text("Successfully Restarted\nPlease wait 1-2 min for loading user plugins...")
+    logger.info("Bot is restarting...")
+    os.system(f"kill -9 {os.getpid()} && python3 main.py")
+        
