7f9ca167f55f0d93a62ebab9b13299fcb0470538
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-11 09:02:54 +0530

    Update __main__.py

--- Testing/__main__.py
+++ Testing/__main__.py
@@ -1,9 +1,11 @@
 import logging
 import asyncio
 import importlib
-from Testing import app
+from Testing import app, Bad
 from pyrogram import idle
 from Testing.Modules import ALL_MODULES
+from telethon import TelegramClient
+import Config
 
 # LOGGER HANDLER
 logging.basicConfig(
@@ -18,19 +20,23 @@ logging.basicConfig(
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
+
 # MAIN FUNCTION
 async def main():
     await app.start()
+    await Bad.start()
     for all_module in ALL_MODULES:
         importlib.import_module("Testing.Modules" + all_module)
     LOGGER("Testing.Modules").info("Successfully Imported Modules...")
     LOGGER("Testing").info("Bot Started Successfully...")
     await idle()
     await app.stop()
+    await Bad.disconnect()
     LOGGER("Testing").info("Stopping Bot...")
 
 if __name__ == "__main__":
