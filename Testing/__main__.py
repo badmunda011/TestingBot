a76891a478865c11faa1c7f43860bfb52c9d9e67
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-11 11:45:32 +0530

    Update __main__.py

--- Testing/__main__.py
+++ Testing/__main__.py
@@ -1,10 +1,30 @@
+import logging
 import asyncio
 import importlib
 from Testing import app, Bad
+from pyrogram import idle
 from Testing.Modules import ALL_MODULES
+from telethon import TelegramClient
 import Config
 
-from .logging import LOGGER
+# LOGGER HANDLER
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
 
 # MAIN FUNCTION
 async def main():
@@ -14,10 +34,10 @@ async def main():
         importlib.import_module("Testing.Modules" + all_module)
     LOGGER("Testing.Modules").info("Successfully Imported Modules...")
     LOGGER("Testing").info("Bot Started Successfully...")
-    
-    # Add a delay or wait for an event
-    await asyncio.sleep(60)  # Sleep for 60 seconds
-
+    await idle()
     await app.stop()
     await Bad.disconnect()
     LOGGER("Testing").info("Stopping Bot...")
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
