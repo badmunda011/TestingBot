62f10626bfa390b8df45bec11556ca7eb0a1e01d
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-11 11:04:10 +0530

    Update __main__.py

--- Testing/__main__.py
+++ Testing/__main__.py
@@ -7,24 +7,6 @@ from Testing.Modules import ALL_MODULES
 from telethon import TelegramClient
 import Config
 
-# LOGGER HANDLER
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        logging.FileHandler("log.txt"),
-        logging.StreamHandler(),
-    ],
-)
-
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
-
 
 # MAIN FUNCTION
 async def main():
