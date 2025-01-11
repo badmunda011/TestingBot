cb6edf698355bc55413d736ede17cc06cd9eb24d
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-11 11:04:21 +0530

    Update logging.py

--- Testing/logging.py
+++ Testing/logging.py
@@ -1,1 +1,17 @@
+# LOGGER HANDLER
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
+)
 
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
