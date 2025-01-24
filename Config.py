9656519afbe13b2d379fd6812541d849dfdbac1f
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-01-24 18:23:18 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,4 +8,4 @@ APP_ID = 25742938
 # Get this value from my.telegram.org/apps
 HASH_ID = "b35b715fe8dc0a58e8048988286fc5b6"
 # Get your token from @BotFather on Telegram.
-TOKEN = "7280541678:AAE7Or_mN10MPV2gjEi_JjK3gaFx3XmoRnk"
+TOKEN = "7280541678:AAHWXTNqehfQYuYT-WbSOmPKNZJKCdMwm2E"
