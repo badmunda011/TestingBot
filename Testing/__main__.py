24e1a8bf01dd00041113f75217acc9e0ecf81d88
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-02-12 18:44:51 +0530

    Update __main__.py

--- Testing/__main__.py
+++ Testing/__main__.py
@@ -1,7 +1,7 @@
 import logging
 import asyncio
 import importlib
-from Testing import app, Bad, Sukh
+from Testing import app, Bad
 from pyrogram import idle
 from Testing.Modules import ALL_MODULES
 from telethon import TelegramClient
@@ -30,9 +30,6 @@ def LOGGER(name: str) -> logging.Logger:
 async def main():
     await app.start()
     await Bad.start()
-    await Sukh.initialize()
-    await Sukh.start()
-    await Sukh.run_polling()
     for all_module in ALL_MODULES:
         importlib.import_module("Testing.Modules" + all_module)
     LOGGER("Testing.Modules").info("Successfully Imported Modules...")
@@ -40,8 +37,7 @@ async def main():
     await idle()
     await app.stop()
     await Bad.disconnect()
-    await Sukh.stop()
     LOGGER("Testing").info("Stopping Bot...")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
